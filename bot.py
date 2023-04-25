05b2fbd026f6480c1c08ab6a7de2d7a12308f8f7
kambhani <63814718+kambhani@users.noreply.github.com>
2023-04-25 16:40:02 -0400

    2nd test commit for .env file

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
 import discord
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
+CR_API_TOKEN = os.getenv("CR_API_TOKEN")
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to discord!')
+
+
+client.run(DISCORD_BOT_TOKEN)
